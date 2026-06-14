35928023be705747609b234709f5dcd96bb85320
CosmoNet.info <admindany@gmail.com>
2026-06-14 02:39:25 +0200

    Remove unused NextRequest import

--- src/app/auth/signout/route.ts
+++ src/app/auth/signout/route.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@/utils/supabase/server'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
-import { NextRequest } from 'next/server'
+
 
 export async function POST() {
   const supabase = createClient()
