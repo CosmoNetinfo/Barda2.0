68de6e4cccfa2c47540fcc305a95d704eaef8fa6
CosmoNet.info <admindany@gmail.com>
2026-06-14 02:38:11 +0200

    Fix ESLint warnings for unused variables

--- src/app/auth/signout/route.ts
+++ src/app/auth/signout/route.ts
@@ -3,7 +3,7 @@ import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 import { NextRequest } from 'next/server'
 
-export async function POST(req: NextRequest) {
+export async function POST() {
   const supabase = createClient()
   await supabase.auth.signOut()
 
